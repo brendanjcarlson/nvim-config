004b5a7ca21abc8e2c242fe63186855f960eb957
brendanjcarlson <me@brendanjcarlson.com>
2025-09-15 13:31:59 -0500

    fix templ and treesitter

--- lua/brendanjcarlson/lazy/treesitter.lua
+++ lua/brendanjcarlson/lazy/treesitter.lua
@@ -19,10 +19,20 @@ return {
 				"rust",
 				"templ",
 				"html",
+				"css",
 			},
 			sync_install = false,
 			auto_install = true,
 			additional_vim_regex_highlighting = false,
+			ignore_install = {},
+			modules = {},
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "templ",
+			callback = function()
+				vim.treesitter.start()
+			end,
 		})
 	end,
 }
