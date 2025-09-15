fbced295647336aeea03654f189d484fa89f17bb
brendanjcarlson <me@brendanjcarlson.com>
2025-09-15 09:30:15 -0500

    change colorscheme to catppuccin

--- lua/brendanjcarlson/lazy/colorscheme.lua
+++ lua/brendanjcarlson/lazy/colorscheme.lua
@@ -1,9 +1,8 @@
 return {
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function()
-			vim.cmd("colorscheme rose-pine")
-		end,
-	},
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		vim.cmd("colorscheme catppuccin")
+	end,
 }
