28674bc56f9ed79325c1bf170f9859150fffc29d
brendanjcarlson <me@brendanjcarlson.com>
2025-09-15 13:03:23 -0500

    diagnostic float styles and lsp remaps

--- lua/brendanjcarlson/lazy/lsp.lua
+++ lua/brendanjcarlson/lazy/lsp.lua
@@ -9,15 +9,6 @@ return {
 
 	opts = {
 		servers = {
-			lua_ls = {
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-						},
-					},
-				},
-			},
 			clangd = {},
 			rust_analyzer = {},
 			html = {},
@@ -39,5 +30,34 @@ return {
 			config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
 			lspconfig[server].setup(config)
 		end
+
+		vim.diagnostic.config({
+			float = {
+				focusable = false,
+				style = "minimal",
+				border = "rounded",
+				source = true,
+				header = "",
+				prefix = "",
+			},
+		})
+
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(e)
+				local o = { buffer = e.buf }
+				vim.keymap.set("n", "gd", function()
+					vim.lsp.buf.definition()
+				end, o)
+				vim.keymap.set("n", "K", function()
+					vim.lsp.buf.hover()
+				end, o)
+				vim.keymap.set("n", "<leader>ca", function()
+					vim.lsp.buf.code_action()
+				end, o)
+				vim.keymap.set("n", "<leader>rn", function()
+					vim.lsp.buf.rename()
+				end, o)
+			end,
+		})
 	end,
 }
