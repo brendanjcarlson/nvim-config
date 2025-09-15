aa6ab4103db67ba337e5dbaed8d0f81f1635990a
brendanjcarlson <me@brendanjcarlson.com>
2025-09-15 14:18:57 -0500

    switch to different go adapter and add neotest remaps

--- lua/brendanjcarlson/lazy/neotest.lua
+++ lua/brendanjcarlson/lazy/neotest.lua
@@ -5,13 +5,39 @@ return {
 		"nvim-lua/plenary.nvim",
 		"antoinemadec/FixCursorHold.nvim",
 		"nvim-treesitter/nvim-treesitter",
-		"nvim-neotest/neotest-go",
+		"fredrikaverpil/neotest-golang",
 	},
 	config = function()
 		require("neotest").setup({
 			adapters = {
-				require("neotest-go"),
+				require("neotest-golang"),
 			},
 		})
+
+		vim.keymap.set("n", "<leader>tv", function()
+			require("neotest").summary.toggle()
+		end)
+
+		vim.keymap.set("n", "<leader>tr", function()
+			require("neotest").run.run({
+				suite = false,
+				testify = true,
+			})
+		end)
+
+		vim.keymap.set("n", "<leader>ts", function()
+			require("neotest").run.run({
+				suite = true,
+				testify = true,
+			})
+		end)
+
+		vim.keymap.set("n", "<leader>ta", function()
+			require("neotest").run.run(vim.fn.getcwd())
+		end)
+
+		vim.keymap.set("n", "<leader>to", function()
+			require("neotest").output.open()
+		end)
 	end,
 }
