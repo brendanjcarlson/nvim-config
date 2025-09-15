e836ce8bfa5c654f2ea5d13901480dfadfb14a1d
brendanjcarlson <me@brendanjcarlson.com>
2025-09-15 13:08:39 -0500

    add current work grep to telescope config

--- lua/brendanjcarlson/lazy/telescope.lua
+++ lua/brendanjcarlson/lazy/telescope.lua
@@ -9,6 +9,9 @@ return {
 
 		vim.keymap.set("n", "<leader>pf", builtin.find_files)
 		vim.keymap.set("n", "<leader>ps", builtin.live_grep)
+		vim.keymap.set("n", "<leader>pw", function()
+			builtin.grep_string({ search = vim.fn.expand("<cword>") })
+		end)
 		vim.keymap.set("n", "<leader>vh", builtin.help_tags)
 	end,
 }
